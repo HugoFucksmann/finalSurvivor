af70e2d3ea98dd3631c16a1ed4edc95b16a82648
HugoFucksmann <hugoffuksmann@gmail.com>
2024-10-29 14:04:08 -0300

    first

---
+++ enemys/enemy.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+ 
+var player_reference : CharacterBody2D
+var direction : Vector2
+var speed : float = 75
+var damage : float = 0
+var knockback : Vector2
+var elite : bool = false:
+	set(value):
+		elite = value
+		if value:
+			$Sprite2D.material = load("res://effects/rainbow.tres")
+			scale = Vector2(1.5,1.5)
+ 
+var type : Enemy:
+	set(value):
+		type = value
+		$Sprite2D.texture = value.texture
+		damage = value.damage
+ 
+func _physics_process(delta):
+	var separation = (player_reference.position - position).length()
+	if separation >= 500 and not elite:
+		queue_free()
+ 
+	velocity = (player_reference.position - position).normalized() * speed
+	knockback = knockback.move_toward(Vector2.ZERO, 1)
+	velocity += knockback
+	var collider = move_and_collide(velocity * delta)
+	
+ 
