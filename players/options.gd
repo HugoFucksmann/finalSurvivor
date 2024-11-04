779778680088dbdcb3637ea5c6fc5210d29b2292
HugoFucksmann <hugoffuksmann@gmail.com>
2024-11-04 14:26:46 -0300

    almost ended

--- players/options.gd
+++ players/options.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 @export var weapons: HBoxContainer
+@export var passive_items: HBoxContainer
 var OptionSlot = preload("res://Ui/option_slot.tscn")
 
 @export var particles : GPUParticles2D
@@ -17,17 +18,25 @@ func close_option():
 	panel.hide()
 	get_tree().paused = false
 	
-func get_aviable_weapons():
-	var weapon_resource = []
-	for weapon in weapons.get_children():
-		if weapon.weapon != null:
-			weapon_resource.append(weapon.weapon)
-	return weapon_resource
+func get_aviable_resource_in(items) -> Array[Item]:
+	var resurces : Array[Item] = []
+	for item in items.get_children():
+		if item.item != null:
+			resurces.append(item.item)
+	return resurces
 	
-
+func add_option(item) -> int:
+	if item.is_upgradable():
+		var option_slot = OptionSlot.instantiate()
+		option_slot.item = item
+		add_child(option_slot)
+		return 1
+	return 0
+	
 func show_option():
-	var weapons_aviable = get_aviable_weapons()
-	if weapons_aviable.size() == 0:
+	var weapons_aviable = get_aviable_resource_in(weapons)
+	var passive_item_aviable = get_aviable_resource_in(passive_items)
+	if weapons_aviable.size() == 0 and passive_item_aviable.size() == 0:
 		return
 		
 	for slot in get_children():
@@ -35,12 +44,17 @@ func show_option():
 		
 	var option_size = 0
 	for weapon in weapons_aviable:
-		if weapon.is_upgradable():
+		option_size += add_option(weapon)
+		
+		if weapon.max_level_reached() and weapon.item_needed in passive_item_aviable:
 			var option_slot = OptionSlot.instantiate()
-			option_slot.weapon = weapon
+			option_slot.item = weapon
 			add_child(option_slot)
-			option_size += 1
-
+			option_slot += 1
+		
+	for passive_item in passive_item_aviable:
+		option_size += add_option(passive_item)
+			
 	if option_size == 0:
 		return
 	show()
--- players/player.gd
+++ players/player.gd
@@ -1,13 +1,30 @@
 extends CharacterBody2D
  
-var speed : float = 150
+
 var health : float = 100.0 :
 	set(value):
-		health = value
+		health = max(value, 0)
 		%Health.value = value
 		
+var max_health : float = 100.0 :
+	set(value):
+		max_health = value
+		%Health.max_value = value
+		
+var recovery : float = 0
+var area : float = 0
+var movement_speed : float = 150
 var nearest_enemy : CharacterBody2D
-var nearest_enemy_distance : float = INF
+var nearest_enemy_distance : float = 150 + area
+var armor : float = 0
+var might : float = 1.5
+
+var magnet : float = 0:
+	set(value):
+		magnet = value
+		%Magnet.shape.radius = 50 + value
+
+var growth : float = 1
 
 var XP : int = 0:
 	set(value):
@@ -23,6 +40,8 @@ var level: int = 1:
 			%XP.max_value = 20
 		elif level >= 7:
 			%XP.max_value = 40
+
+
 		
 @onready var camera = $Camera2D
 var camera_smoothing_speed = 10.0
@@ -36,16 +55,19 @@ func _physics_process(delta):
 		nearest_enemy_distance = nearest_enemy.separation
 		print(nearest_enemy.name)
 	else:
-		nearest_enemy_distance = INF	
-	velocity = Input.get_vector("left","right","up","down") * speed
+		nearest_enemy_distance = 150 + area
+		nearest_enemy = null
+	velocity = Input.get_vector("left","right","up","down") * movement_speed
 	move_and_collide(velocity * delta)
 	check_XP()
+	health += recovery * delta 
 	var target_position = global_position
 	target_position.x = clamp(target_position.x, camera_limits.position.x, camera_limits.end.x)
 	target_position.y = clamp(target_position.y, camera_limits.position.y, camera_limits.end.y)
 	camera.global_position = camera.global_position.lerp(target_position, delta * camera_smoothing_speed)
 	camera.global_position += camera_pan_velocity * delta
 	camera_pan_velocity = camera_pan_velocity.move_toward(Vector2.ZERO, camera_pan_speed * delta)
+	
 	
 #	if velocity == Vector2.ZERO:
 #		$AnimationPlayer.play("idle")
@@ -58,7 +80,7 @@ func _physics_process(delta):
 		$Sprite2D.flip_h = false
  
 func take_damage(amount):
-	health -= amount
+	health -= max(amount - armor, 0)
 	print(amount)
  
 func _on_self_damage_body_entered(body):
@@ -69,14 +91,13 @@ func _on_timer_timeout() -> void:
 	%Collision.set_deferred("disabled",false)
 
 func gain_XP(amount):
-	XP += amount
-	total_XP += amount
+	XP += amount * growth
+	total_XP += amount * growth
 
 func check_XP():
 	if XP > %XP.max_value:
 		XP -= %XP.max_value
 		level += 1
-
 
 func _on_magnes_area_entered(area: Area2D) -> void:
 	if area.has_method("follow"):
