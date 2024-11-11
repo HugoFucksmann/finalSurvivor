dcdaca7c79dd79a6b5aae8d2ce3b0671d0b8c78f
HugoFucksmann <hugoffuksmann@gmail.com>
2024-11-11 13:40:08 -0300

    mejora ui

--- Ui/upgrade.gd
+++ Ui/upgrade.gd
@@ -6,7 +6,17 @@ var enabled : bool = false:
 	set(value):
 		enabled = value
 		$Panel.show_behind_parent = value
-
+		
+		if value:
+			$OutLine.add_point(Vector2(0,-1))
+			$OutLine.add_point(Vector2(40,-1))
+			$OutLine.add_point(Vector2(40,39))
+			$OutLine.add_point(Vector2(0,39))
+		if value and get_index() != 0:
+			$Conection.add_point(Vector2(20,20) + initial_modifier())
+			$Conection.add_point(Vector2(20,20))
+			$Conection.add_point(get_parent().get_child(get_index() -1).position - position + Vector2(20,20) + final_modifier()) 
+			
 
 func _ready():
 	if skill:
@@ -29,3 +39,37 @@ func _on_pressed() -> void:
 		enabled = true
 		get_parent().get_parent().set_skill_tree()
 		get_parent().get_parent().get_total_stats()
+
+
+func initial_modifier() -> Vector2:
+	#SoundManager.play_sfx(load("RES:"))
+	var difference = get_parent().get_child(get_index() -1).position - position
+	var modification : Vector2 = Vector2.ZERO
+	
+	if difference.x < 0:
+		modification += Vector2(-20,0)
+	elif difference.x > 0:
+		modification += Vector2(20,0)
+		
+	if difference.y < 0:
+		modification += Vector2(0,-20)
+	elif difference.y > 0:
+		modification += Vector2(0,20)
+	
+	return modification
+
+func final_modifier() -> Vector2:
+	var difference = get_parent().get_child(get_index() -1).position - position
+	var modification : Vector2 = Vector2.ZERO
+	
+	if difference.x < 0:
+		modification += Vector2(20,0)
+	elif difference.x > 0:
+		modification += Vector2(-20,0)
+		
+	if difference.y < 0:
+		modification += Vector2(0,20)
+	elif difference.y > 0:
+		modification += Vector2(0,-20)
+	
+	return modification
